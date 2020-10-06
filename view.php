78e0863a6e884297df18f8b03e43e9bfc512ac2f
Na-naO <la.dolce.vita.nana@gmail.com>
2020-10-06 22:11:03 +0900

    initiate folder

---
+++ view.php
@@ -0,0 +1,46 @@
+<?php
+// １．データベースに接続する
+$dsn = 'mysql:dbname=phpkiso;host=localhost';
+$user = 'root';
+$password = '';
+$dbh = new PDO($dsn, $user, $password);
+$dbh->query('SET NAMES utf8');
+
+// ２．SQL文を実行する
+$sql = 'SELECT * FROM `survey`';
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+
+
+
+
+
+// while (1) {
+//   $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+//   if ($rec == false) {
+//     break;
+//   }
+//   echo $rec['code'] . '<br>';
+//   echo $rec['nickname'] . '<br>';
+//   echo $rec['email'] . '<br>';
+//   echo $rec['content'] . '<br>';
+//   echo '<hr>';
+// }
+
+// 配列×foreach
+// fetchallを使用。
+$recs = $stmt-> fetchall();
+// var_dump($rec);
+
+foreach($recs as $rec);
+echo $rec['code'] . '<br>';
+  echo $rec['nickname'] . '<br>';
+  echo $rec['email'] . '<br>';
+  echo $rec['content'] . '<br>';
+  echo '<hr>';
+
+
+
+// ３．データベースを切断する
+$dbh = null;
